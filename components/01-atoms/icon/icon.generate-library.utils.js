f411c194411f93c8d935d87ecea7994f7f81ffc7
Deployer Robot <deployer@salsadigital.com.au>
2022-03-14 22:11:08 +1100

    Release 0.39.0.
    Dsisabled debug.
    Added creation of user accounts and fixed site email. (#3)

--- components/01-atoms/icon/icon.generate-library.utils.js
+++ components/01-atoms/icon/icon.generate-library.utils.js
@@ -1,12 +1,13 @@
-//
-// Generate icon library file from icon files.
-//
+/**
+ * @file
+ * Generate icon library file from icon files.
+ */
 
 const fs = require('fs');
 const iconUtils = require('./icon.utils');
 
 // Path to the output icon library file.
-const iconLibraryPath = './components/01-atoms/icon/icon_library.twig';
+const iconLibraryPath = './components/01-atoms/icon/icon_library.generated.twig';
 
 function getMatches(reg, text, matchIndex) {
   const matches = [];
